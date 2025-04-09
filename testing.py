4e74639ff8c0abc06e8a5883059ace346156e07b
Rahil Kothari <kotharirahil9@gmail.com>
2025-04-09 15:04:30 +0530

    feat: create Twilio integration

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://localhost:5000/predict"
+url = "https://selected-glowworm-gradually.ngrok-free.app/predict" 
 files = {"file": open("./New Test/0bf809ec-841e-4668-b875-adeb73d60de0___FAM_B.Msls 4057.JPG", "rb")}  # replace image.jpg with your image file path
 response = requests.post(url, files=files)
 print(response.json())
